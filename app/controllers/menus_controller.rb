f37e5b286c2e6e290e32abbdd35a50441768125f
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-06-02 20:25:54 -0700

    Added comments and styling to form

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,28 +1,31 @@
 class MenusController < ApplicationController
 	def index
 		base_url = "https://api.appery.io/rest/1/db/collections/Menu/"
+
+		# get request to retrieve data
 		@items = HTTParty.get(base_url,
 			:headers => {"X-Appery-Database-Id" => "5476a8c5e4b05e4a44a69b0e"}).parsed_response
 	end
 
 	def new
-
 	end
 
 	def create
+		# grab item params to be added in post request below
 		item = params[:item]
 		list_id = params[:listid]
 		price = params[:price]
 		num_orders = params[:num_orders]
 
+		# post request to add new item
 		base_url = "https://api.appery.io/rest/1/db/collections/Menu/"
+
 		response = HTTParty.post(base_url, 
 			:body => {"item" => "#{item}", "listid" => "#{list_id}", "price" => "#{price}", "num_orders" => "#{num_orders}"}.to_json, 
 			:headers => {"X-Appery-Database-Id" => "5476a8c5e4b05e4a44a69b0e",
 				"Content-Type" => "application/json"})
 
-		puts "response: #{response}"
-
+		# after request redirect user back to home page
 		redirect_to '/'
 	end
 end
