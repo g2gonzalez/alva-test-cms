fed90f83130d80cc978ec3a3c8dcfb93dab5b58d
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-06-02 20:28:58 -0700

    Added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # home path
   root 'menus#index'
+  # drinks home path
   get '/drinks' => 'drinks#index'
+  # add new item form path
   get '/menus/new' => 'menus#new'
+  # send form data path
   post '/menus' => 'menus#create'
 end
