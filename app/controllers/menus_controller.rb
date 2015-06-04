7a41d9de8a172a49be47cf4527f0b814b6963758
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-06-03 20:07:35 -0700

    Forms refactored and edit, update actions fixed

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,5 @@
 class MenusController < ApplicationController
+
 	def index
 		base_url = "https://api.appery.io/rest/1/db/collections/Menu/"
 
@@ -7,7 +8,21 @@ class MenusController < ApplicationController
 			:headers => {"X-Appery-Database-Id" => "5476a8c5e4b05e4a44a69b0e"}).parsed_response
 	end
 
+	def show
+	end
+
 	def new
+	end
+
+	def edit
+		# grab item id that needs to be edited
+		id = params[:id]
+
+		base_url = "https://api.appery.io/rest/1/db/collections/Menu/#{id}"
+
+		# grab item selected information
+		@items = HTTParty.get(base_url,
+			:headers => {"X-Appery-Database-Id" => "5476a8c5e4b05e4a44a69b0e"}).parsed_response
 	end
 
 	def create
@@ -29,30 +44,27 @@ class MenusController < ApplicationController
 		redirect_to '/'
 	end
 
-	def edit
-		base_url = "https://api.appery.io/rest/1/db/collections/Menu/556d473fe4b0b40eb778a1ab"
-
-		# grab item selected information
-		@items = HTTParty.get(base_url,
-			:headers => {"X-Appery-Database-Id" => "5476a8c5e4b05e4a44a69b0e"}).parsed_response
-	end
-
 	def update
 		# grab item params to be added in put request below
-		item = params[:item]
-		price = params[:price]
-		num_orders = params[:num_orders]
+		id = params[:id]
+		item = params[:menu][:item]
+		list_id = params[:menu][:listid]
+		num_orders = params[:menu][:num_orders]
+		price = params[:menu][:price]
 
 		# put request to add new item
-		base_url = "https://api.appery.io/rest/1/db/collections/Menu/556d473fe4b0b40eb778a1ab"
+		base_url = "https://api.appery.io/rest/1/db/collections/Menu/#{id}"
 
 		response = HTTParty.put(base_url, 
-			:body => {"item" => "#{item}", "price" => "#{price}", "num_orders" => "#{num_orders}"}.to_json, 
+			:body => {"item" => "#{item}", "listid" => "list_id", "num_orders" => "num_orders", "price" => "price"}.to_json, 
 			:headers => {"X-Appery-Database-Id" => "5476a8c5e4b05e4a44a69b0e",
 				"Content-Type" => "application/json"})
 
 		# after request redirect user back to home page
 		redirect_to '/'
+	end
+
+	def delete
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,8 @@ Rails.application.routes.draw do
   root 'menus#index'
   # drinks home path
   get '/drinks' => 'drinks#index'
-  # add new item form path
-  get '/menus/new' => 'menus#new'
-  # send form data path
-  post '/menus' => 'menus#create'
-  # edit form path
-  get '/menus/edit' => 'menus#edit'
   # update form data path
   post '/menus/update' => 'menus#update'
+ 
+  resources :menus
 end
